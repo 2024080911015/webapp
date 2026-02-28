3a05152e8580ffe925728a86f2eb4036b802c014
找不到方向的指针 <13271722058@163.com>
2026-02-28 12:29:39 +0800

    开凭与跳转雏形

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def start():
     return render_template('index.html')
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
